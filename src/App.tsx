7d36f1d6a6a5bca703bb61808a32431da40a6cea
Gabriel Dantas <gabrielmd123@gmail.com>
2022-05-24 22:43:48 -0300

    widget main component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { WidgetComponent } from "./components/WidgetComponent";
+
 export function App() {
   return (
-    <h1 className="bg-purple-500 p-2">Hello World</h1>
+    <WidgetComponent/>
   )
 }
